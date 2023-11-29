31f919bdf885fbba0afad968df5fdb50258475ec
markdrahg <neutral520@gmail.com>
2023-11-28 18:05:53 -0800

    102-magic_calculation.py

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for char in range(122, 96, -1):
-    case = char - 32 if (char - 122) % 4 == 0 else char
-    print("{:c}".format(case), end='')
+for char in range(122, 96, -2):
+    print("{:c}{:c}".format(char, char - 33), end='')
+
