ff6678f8cce62e8a3b3b1476882d61f91e92039f
Mark Drah <132355775+markdrahg@users.noreply.github.com>
2024-04-05 20:32:17 +0000

    Update 0-privileges.sql

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,5 @@
-sts all privileges of the MySQL users user_0d_1 and user_0d_2
+-- List privileges for user_0d_1
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
+
+-- List privileges for user_0d_2
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
