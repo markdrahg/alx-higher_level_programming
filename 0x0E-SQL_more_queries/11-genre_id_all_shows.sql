78f51d0cae3f0b79ea742be798d6f562cb971b30
Mark Drah <132355775+markdrahg@users.noreply.github.com>
2024-04-05 20:30:09 +0000

    Update 11-genre_id_all_shows.sql

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,5 +1,5 @@
--- Select tv_shows.title and tv_show_genres.genre_id from tv_shows left join tv_show_genres tables
+-- Select tv_shows.title and tv_show_genres.genre_id from tv_shows and tv_show_genres tables
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
-LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
-ORDER BY tv_shows.title ASC, COALESCE(tv_show_genres.genre_id, 0) ASC;
+JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
