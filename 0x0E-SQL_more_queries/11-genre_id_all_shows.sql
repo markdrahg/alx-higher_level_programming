762db625ab831230726a20c09aecea0b5ca2f16e
Mark Drah <132355775+markdrahg@users.noreply.github.com>
2024-04-05 20:25:46 +0000

    Update 11-genre_id_all_shows.sql

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,6 +1,5 @@
-sts all shows contained in the database hbtn_0d_tvshows
--- lists all rows of tables in a database
+-- Select tv_shows.title and tv_show_genres.genre_id from tv_shows left join tv_show_genres tables
 SELECT tv_shows.title, tv_show_genres.genre_id
-FROM tv_shows LEFT JOIN tv_show_genres
-ON tv_shows.id = tv_show_genres.show_id
-ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
+FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title ASC, COALESCE(tv_show_genres.genre_id, 0) ASC;
