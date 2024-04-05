06b899cb52b80625d9eb418246927344bdb4b8a4
Mark Drah <132355775+markdrahg@users.noreply.github.com>
2024-04-05 20:34:37 +0000

    Update 2-create_read_user.sql

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,7 +1,8 @@
-tes the database hbtn_0d_2 and the user user_0d_2
--- creates a database
+-- Create database hbtn_0d_2 if it does not exist
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
--- creates a user
-CREATE USER IF NOT EXISTS user_0d_2@localhost IDENTIFIED BY 'user_0d_2_pwd';
--- grants SELECT privileges to a user
-GRANT SELECT ON hbtn_0d_2.* TO user_0d_2@localhost;
+
+-- Create user user_0d_2 if it does not exist
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+
+-- Grant SELECT privilege on database hbtn_0d_2 to user_0d_2
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
