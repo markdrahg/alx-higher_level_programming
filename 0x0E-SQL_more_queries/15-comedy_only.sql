c168535f8ea2511bba4396ff8df596d0ea07066c
Mark Drah <132355775+markdrahg@users.noreply.github.com>
2024-04-05 20:21:39 +0000

    Update 15-comedy_only.sql

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -1,9 +1,7 @@
-sts all Comedy shows in the database hbtn_0d_tvshows
--- lists all rows of a database corresponding to a column value
-SELECT title
+-- Select tv_shows.title from tv_shows join tv_show_genres join tv_genres tables
+SELECT tv_shows.title
 FROM tv_shows
-LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
-LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
 WHERE tv_genres.name = 'Comedy'
-GROUP BY title
-ORDER BY title ASC;
+ORDER BY tv_shows.title ASC;
