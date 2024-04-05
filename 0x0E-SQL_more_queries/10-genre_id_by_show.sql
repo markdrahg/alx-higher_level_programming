d058a253ccac14dae728c98fcd41e8ceb99a304b
Mark Drah <132355775+markdrahg@users.noreply.github.com>
2024-04-05 20:47:47 +0000

    Update 10-genre_id_by_show.sql

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,3 +1,5 @@
-sts all shows contained in hbtn_0d_tvshows that have at least one genre linked
--- lists all rows of a database that have one column in common
-SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows INNER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
+-- Select tv_shows.title and tv_show_genres.genre_id from tv_shows and tv_show_genres tables
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
