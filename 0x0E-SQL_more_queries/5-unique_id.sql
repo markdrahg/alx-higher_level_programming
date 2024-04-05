e61f4c65ed3e46c0d340712cdba1a48fefd28fef
Mark Drah <132355775+markdrahg@users.noreply.github.com>
2024-04-05 20:42:12 +0000

    Update 5-unique_id.sql

--- 0x0E-SQL_more_queries/5-unique_id.sql
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -1,3 +1,5 @@
-tes the table unique_id on your MySQL server
--- creates a table
-CREATE TABLE IF NOT EXISTS unique_id (id INT DEFAULT 1 UNIQUE, name VARCHAR(256));
+-- Create the table unique_id if it does not exist
+CREATE TABLE IF NOT EXISTS unique_id (
+    id INT DEFAULT 1 UNIQUE,
+    name VARCHAR(256)
+);
