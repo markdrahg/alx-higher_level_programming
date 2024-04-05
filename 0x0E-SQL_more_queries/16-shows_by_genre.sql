c4507390eb113ffd3d8b7b889e1b280550fa39f1
Mark Drah <132355775+markdrahg@users.noreply.github.com>
2024-04-05 20:20:02 +0000

    Update 16-shows_by_genre.sql

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -1,7 +1,6 @@
-sts all shows, and all genres linked to that show, from the database hbtn_0d_tvshows
--- lists all rows of a table linked to another table
-SELECT title, name
+-- Select tv_shows.title and tv_genres.name from tv_shows left join tv_show_genres join tv_genres tables
+SELECT tv_shows.title, tv_genres.name
 FROM tv_shows
 LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
-ORDER BY title ASC, name ASC;
+ORDER BY tv_shows.title ASC, tv_genres.name ASC;
