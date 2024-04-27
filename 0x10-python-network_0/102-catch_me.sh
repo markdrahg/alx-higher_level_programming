5da1ccdda5d83124dbd4114c439e72969af15db0
Mark Drah <132355775+markdrahg@users.noreply.github.com>
2024-04-27 12:00:36 +0000

    Update 102-catch_me.sh

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 
-# Make a request to 0.0.0.0:5000/catch_me
-response_code=$(curl --write-out "%{http_code}\\n" "http://0.0.0.0:5000/catch_me")
-
-# Check if the response code is 200 (successful)
-if [ "$response_code" -eq 200 ]; then
-    echo "You got me!"
-fi
+# Send a PUT request to 0.0.0.0:5000/catch_me with curl
+curl -s -X PUT 0.0.0.0:5000/catch_me --data "user_id=98" --header "Origin: HolbertonSchool" --header "Referer: HolbertonSchool" 
